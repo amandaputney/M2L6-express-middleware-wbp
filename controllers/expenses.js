e153d317a3a4f0d4b93b90df3d44f0e62c2e1312
asp <amandasueputney@gmail.com>
2023-07-15 18:51:28 -0700

    completes step1 & 2

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,22 +1,26 @@
 module.exports = {
   index,
   show,
+  new: newExpense
 };
 
 const Expense = require('../models/expense');
 
 function index(req, res) {
   const expenses = Expense.getAll();
-
   res.render('expenses/index', {
     title: 'Expense Report',
     expenses
   });
+  ;
 }
+
+function newExpense(req, res) {
+  res.render('expenses/new', { title: 'New Expense' })
+};
 
 function show(req, res) {
   const expense = Expense.getOne(req.params.id);
-
   res.render('expenses/show', {
     title: 'Expense Details',
     expense
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const expensesCtrl = require('../controllers/expenses');
+const { route } = require('.');
 
 router.get('/', expensesCtrl.index);
+
+router.get('/new', expensesCtrl.new);
 
 router.get('/:id', expensesCtrl.show);
 
